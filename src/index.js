af77f19cd585a1f94af3d95a4a58ff46a2d62525
peter <sunghnkim95@gmail.com>
2021-12-09 20:39:16 -0500

    css and react not showing

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+//import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+
+const App = () => {
+    return <div>hello</div>
+}
+ReactDOM.render(<App />, document.getElementById('app'));
